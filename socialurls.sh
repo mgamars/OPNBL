3f3674a1c849232736d0da962409b0daf3e5dcd0
amtn <amrimed@gmail.com>
2019-04-12 22:50:44 +0100

    Update socialurls.sh

--- socialurls.sh
+++ socialurls.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
+
 ###############################################################################
-# Copyright 2018-2019 Amri Mohamed                                            #
+# The MIT License (MIT)                                                       #
 #                                                                             #
-#    Licensed under the GNU GPLv3 (the "License"); you may                    #
-#    not use this file except in compliance with the License. You may obtain  #
-#    a copy of the License at                                                 #
+# Copyright (c) 2016 Sinfonietta                                              #
 #                                                                             #
-#         https://www.gnu.org/licenses/gpl.html                               #
-#                                                                             #
+# Copyright 2018-2019 amtn                                                    #
 # file name: socialurls.sh                                                    #
 # this script will perform a download and formatting of a hosts file          #
 # containing a list of social networks URLs                                   #
@@ -20,7 +18,7 @@ echo "Removing old files..."
 [ -f /tmp/hosts.working ] && rm -f /tmp/hosts.working
 
 whitelist='127.0.0.1'
-blacklist='http://winhelp2002.mvps.org/hosts.txt https://adaway.org/hosts.txt http://sysctl.org/cameleon/hosts https://hosts-file.net/ad_servers.txt https://hosts-file.net/grm.txt https://hosts-file.net/hfs.txt https://hosts-file.net/mmt.txt https://hosts-file.net/emd.txt https://hosts-file.net/fsa.txt https://hosts-file.net/exp.txt https://hosts-file.net/hjk.txt https://hosts-file.net/psh.txt https://hosts-file.net/pha.txt http://pgl.yoyo.org/as/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext'
+blacklist='https://raw.githubusercontent.com/Sinfonietta/hostfiles/master/social-hosts'
 
 ## Fetch all Blacklist Files
 echo "Fetching Blacklists..."
@@ -29,13 +27,14 @@ for url in $blacklist; do
 done
 
 ## Make dir if not exists
-mkdir -p /usr/local/www/myblacklists
+#mkdir -p /usr/local/www/myblacklists
 
 ## Process Blacklist, Eliminiating Duplicates, Integrating Whitelist, and 
 ## Converting to URL Table format
 awk -v whitelist="$whitelist" '$1 ~ /^127\.|^0\./ && $2 !~ whitelist {gsub("\r",""); \
 print tolower($2)}' /tmp/hosts.working | sort | uniq | \
-awk '{printf $1; printf "\n"}' > /usr/local/www/myblacklists/socialurls
+awk '{printf $1; printf "\n"}' > ./socialurls
+##awk '{printf $1; printf "\n"}' > /usr/local/www/myblacklists/socialurls
 
 
 echo "Cleaning Up..."
